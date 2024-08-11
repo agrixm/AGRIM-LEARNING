e4f65367a654b01677b22b3d7cc826252e932536
agrixm <sagrim510@gmail.com>
2024-08-12 04:12:41 +0530

    day-1

---
+++ first.py
@@ -0,0 +1,32 @@
+# x = 0
+# if x >  10 :
+#     print( "x is greater than 10" )
+# elif x < 10 :
+#     print( "x is less than 10" )
+# else :
+#     print( "x is equal to 10" )
+
+
+# while x < 10:
+#     print(x)
+#     x += 1
+
+# arr = ['oaisjd','v','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o']
+
+# for i in range(len(arr)):
+#     print(i)
+
+
+arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+dic = {
+    "name": "John",
+    "aoisdj": ['123',123]
+}
+
+tup = (1, 2,3,4)
+
+
+print(dic['name'])
+
+
