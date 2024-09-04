b75aa46b3933f9c38f1f5bf48c339d120b825b42
agrixm <sagrim510@gmail.com>
2024-09-05 01:36:34 +0530

    Day-3

---
+++ third.py
@@ -0,0 +1,29 @@
+a=50
+b=3
+
+print("The value of", a, "+", 3, "is: ", a + b)
+print("The value of", a, "-", 3, "is: ", a - b)
+print("The value of", a, "*", 3, "is: ", a * b)
+print("The value of", a, "/", 3, "is: ", a / b)
+
+#Type Casting
+
+num1 = '1'
+num2 = '2'
+print(float(num1) + float(num2))
+print(int(num1) + int(num2))
+
+
+number = "10"
+numeral = 5
+string_number = int(number)
+sum = string_number + numeral
+
+print("sum of both is: ", sum)
+
+c = 7
+d = 8.7
+
+
+print(c + d)
+print(type(c + d))
