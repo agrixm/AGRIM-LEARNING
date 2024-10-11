f77105eb08ae026d868812e4f679ef93efa3b2a5
agrixm <sagrim510@gmail.com>
2024-10-11 23:50:18 +0530

    6

--- sixth.py
+++ sixth.py
@@ -1,28 +1,18 @@
+a="3" 
+b="5"
+print(a+b)
 
-print("Enter the first number: ")
-a=input()
-print("Number is: ", a)
-print("Enter the second number: ")
-b=input()
-print("Number is: ", b)
-print("1. Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division\n 5.Exponentiation\n6.Modulus\n7.Floor Division")
-print("Enter the operation you want to perform: ")
-c=input()
-if c==1:
-    print("The sum of",a,"and",b,"is",a+b)
-elif c==2:
-    print("The difference of",a,"and",b,"is",a-b)
-elif c==3:      
-    print("The product of",a,"and",b,"is",a*b)
-elif c==4:
-    if b!=0:
-        print("The Division of",a,"and",b,"is",a/b)
+q="3"
+w="5"
+print(int(q)+int(w)) 
 
 
-elif c==5:
-    print("The Exponentiation of",a,"and",b,"is",a**b)
-elif c==6:
-    print("The Modulus of",a,"and",b,"is",a%b)
-elif c==7:
-    print("The Floor Division of",a,"and",b,"is",a//b)
-    
+
+a=input("Enter a number: ")
+print("My Name is",a)
+x=input("Enter a number: ")
+y=input("Enter a number: ")
+print(int(x)+int(y))  #typecasting
+
+
+
