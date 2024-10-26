bc6cbff651846d1f8403974d3ec9b4560e6c4553
agrixm <sagrim510@gmail.com>
2024-10-27 00:30:57 +0530

    one more day done

--- ninth.py
+++ ninth.py
@@ -1,16 +1,18 @@
-import time
-timestamp = time.strftime('%D:%H:%M:%S:%j:%w')
-print("The time is",timestamp)
+# import time
+# timestamp = time.strftime('%D:%H:%M:%S:%j:%w')
+# print("The time is",timestamp)
 
-# for loops
+# # for loops
 
-for i in range(1,10):
-    print(i)
+# for i in range(1,10):
+#     print(i)
 
-name=["Agrim","pihu","Shivam","Shubham"]
-for k in name:
-    print(k)
-
+# name=["Agrim","pihu","Shivam","Shubham"]
+# for k in name:
+#     print(k)
+i=0
 while(i<=38):
-   i=int(input("Enter the number:" ))
+   i=int(input("Enter the number:"))
    print(i) 
+   i=i+1 
+print("The loop is over")
