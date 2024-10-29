b3b2aa8a7fe9653247009fb222bb8e6bd0d762f4
agrixm <sagrim510@gmail.com>
2024-10-30 02:45:18 +0530

    List,Tuple and Opearations

---
+++ eleventh.py
@@ -0,0 +1,65 @@
+#lists
+
+l=[1,2,3,4,5,6,7,8,9,10]
+print(type(l))
+print(l[0])
+
+
+
+
+#string-indexing
+a="Hello"
+print(a[0])
+
+
+# list can have multiple data types
+
+l=[1,True,"Hello",3.5]  
+print(typel)
+
+l=["agrim","amy","peter","chris","jill","leon","claire","jake","sherry","ada"]
+print(l[-5])
+print(len(l))
+if "jill" in l:
+    print("Yes")
+else:    
+    print("No")
+print(l[0:5])
+print(l[-6:-5])
+print(l[:5])
+
+
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[3:7])	
+print(animals[-7:-2])	
+
+
+
+#lIST mETHODS
+colors = ["voilet", "indigo", "blue", "green"]
+colors.sort()
+print(colors)
+
+num = [1,2,1,3,4,5]
+
+num.insert(2,3)
+print(num)
+
+colors = ["red","blue"]
+num=[1,2,3]
+colors.extend(num)  
+print(colors)
+
+countries = ("Spain", "Italy", "India", "England", "Germany")
+temp = list(countries)
+temp.append("Russia")       #add item 
+temp.pop(3)                 #remove item
+temp[2] = "Finland"         #change item
+countries = tuple(temp)
+print(countries)
+
+num = (1,2,3,4)
+temp=list(num)
+temp.insert(1,"Green")
+num=tuple(temp)
+print(num)
--- tenth.py
+++ tenth.py
@@ -1,79 +1,78 @@
 #break 
-# for i in range(12):
-#     if(i==7):
-#        print("skip the loop")
-#        continue
-#     print("5 X",i,"=",5*i)
+for i in range(12):
+    if(i==7):
+       print("skip the loop")
+       continue
+    print("5 X",i,"=",5*i)
 
-# for i in [2,3,4,6,8,0]:
-#     if (i%2!=0):
-#         continue
-#     print(i)
+for i in [2,3,4,6,8,0]:
+    if (i%2!=0):
+        continue
+    print(i)
 
-# for i in range(1,101,1):
-#     print(i ,end=" ")
-#     if(i==50):
-#         break
-#     else:
-#         print("Mississippi")
-# print("Thank you")
+for i in range(1,101,1):
+    print(i ,end=" ")
+    if(i==50):
+        break
+    else:
+        print("Mississippi")
+print("Thank you")
 
 #functions
-# def mean(a,b):
-#     print((a*b)/(a+b))
-#     return mean
-# def isGreater(a,b):
-#     if a>b:
-#         print("a is greater than b")
-#     else:
-#         print("b is greater than a")
-# a=9
-# b=8
-# (mean(a,b))
-# (isGreater(a,b))
-# c=7
-# d=8
-# (mean(c,d))
-# (isGreater(c,d))
+def mean(a,b):
+    print((a*b)/(a+b))
+    return mean
+def isGreater(a,b):
+    if a>b:
+        print("a is greater than b")
+    else:
+        print("b is greater than a")
+a=9
+b=8
+(mean(a,b))
+(isGreater(a,b))
+c=7
+d=8
+(mean(c,d))
+(isGreater(c,d))
 
 
 #default argument
-# def average(a=9,b=9):
-#     print("The average of two numbers is",(a+b)/2)
+def average(a=9,b=9):
+    print("The average of two numbers is",(a+b)/2)
 
 
-# average(5)
+average(5)
 
-# def name(fname, mname = "Jhon", lname = "Whatson"):
-#     print("Hello,", fname, mname, lname)
+def name(fname, mname = "Jhon", lname = "Whatson"):
+    print("Hello,", fname, mname, lname)
 
-# name("Amy","agrim")
+name("Amy","agrim")
 
-# def name(fname,lname, mname ):
-#     print("Hello,", fname, lname, mname)
+def name(fname,lname, mname ):
+    print("Hello,", fname, lname, mname)
 
-# name( lname = "Wesker",mname = "Peter" ,fname="Chris")
-# def name(fname, mname):
-#     print("Hello,", fname, mname)
+name( lname = "Wesker",mname = "Peter" ,fname="Chris")
+def name(fname, mname):
+    print("Hello,", fname, mname)
 
-# name("Peter")
+name("Peter")
 
-# def name(*hello):
-#     print("Hello,",hello[0],hello[1],hello[2])
+def name(*hello):
+    print("Hello,",hello[0],hello[1],hello[2])
 
-# name("James", "Buchanan", "Barnes", "Steve", "Rogers")
-
-
-# def name(**name):
-#     print("Hello,", name["fname"], name["mname"], name["lname"])
-
-# name( lname = "Barnes",mname = "Buchanan", fname = "James")
+name("James", "Buchanan", "Barnes", "Steve", "Rogers")
 
 
-#return statement
-# def name(fname, mname, lname):
-#     return "Hello, " + fname +  mname + lname
+def name(**name):
+    print("Hello,", name["fname"], name["mname"], name["lname"])
 
-# print(name("James", "Buchanan"))
+name( lname = "Barnes",mname = "Buchanan", fname = "James")
 
-#list
+
+# return statement
+def name(fname, mname, lname):
+    return "Hello, " + fname +  mname + lname
+
+print(name("James", "Buchanan"))
+
