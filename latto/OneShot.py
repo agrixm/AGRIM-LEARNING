7695f1e27f77902a472a50e39977b3e496657656
agrixm <sagrim510@gmail.com>
2024-11-01 23:09:43 +0530

    Python Revision

---
+++ latto/OneShot.py
@@ -0,0 +1,105 @@
+# import pyjokes
+
+# joke = pyjokes.get_joke()
+# print(joke)
+
+
+#comments
+#jai mata di
+"""jai ho jai
+ baba ji ki"""
+
+#Q1. Twinkle Twinkle Little Star
+# poem="""Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.
+# Twinkle, twinkle, little star,
+# How I wonder what you are!"""
+
+# print(poem)
+
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("Hello Agrim")
+# engine.runAndWait()
+
+# a=1
+# b=2
+# print(a+b)
+
+# a=True
+# print(type(a))
+
+# a="Agrim"
+# print(type(a))
+
+# Agrim=None
+# print(type(Agrim))
+
+# a=1
+# b=5
+# print(a/b)
+# a=2
+# a+=7
+# print(a)
+
+# a=5>6
+# print(a)
+
+# a=True and False
+# print(a)
+
+# print(not False)
+# a="Harry"
+
+# print(type(a))
+
+# a="31.2"
+# print(a)
+# b=float(a)
+# print(type(b))
+
+# a=input("Enter your name: ")
+
+# print("Hello "+a)
+
+# a=int(input("Enter a number: "))
+# b=int(input("Enter a number: "))
+# print("SUM is ",a+b)
+
+
+
+# a=int(input())
+# print(type(a))
+
+# a=int(input("Enter a number 1: "))
+# b=int(input("Enter a number 2: "))
+# print("Sum is ",a+b)
+
+# a=int(input("enter A"))
+# print(5*a)
+
+# a=int(input("Enter the Number"))
+# b=a%2
+# print(b)
+
+# a=input()
+# print(type(a))
+
+# a=float(input())
+# print(type(a))
+
+# a=34
+# b=80
+# print(a>b)
+
+
+# a=int(input("Number A :"))
+# b=int(input("Number B :"))
+# print("Average of Two Numbers is", (a+b)/2)
+
+# a=int(input("Number A :"))
+# print("Square of  Number is", a**2)
+
+
